513d74498706ebccf73c79436bb6beb968c9f545
Antonio Risaldo <antonio.risaldo@gmail.com>
2020-08-09 20:24:05 +0200

    Fixed an issue with incorrect parameter types; Added .ps1 file to assist testing deployments locally;

---
+++ Infrastructure/PowerShell/TestDatabaseDeployment.ps1
@@ -0,0 +1,2 @@
+﻿New-AzResourceGroupDeployment -Name "TestDeploySQL" `
+-ResourceGroupName rg-wow-dev-westeurope `-TemplateFile .\ArmTemplates\composed\Storage\DeployMySql.json `-TemplateParameterFile .\ArmTemplates\composed\Storage\Parameters\DeployMySql.parameters.wow.dev.json -Verbose
