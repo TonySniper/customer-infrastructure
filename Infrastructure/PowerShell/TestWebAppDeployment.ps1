ee107d3799d97cbf72e0893d34b12d0562cf08cc
Antonio Risaldo <antonio.risaldo@gmail.com>
2020-08-12 13:52:38 +0200

    Fixed an issue with parameter files naming; Fixed an issue with an incorrect dependency in the webapp armtemplate;

---
+++ Infrastructure/PowerShell/TestWebAppDeployment.ps1
@@ -0,0 +1,3 @@
+﻿New-AzResourceGroupDeployment -Name "TestDeployWebApp" `
+-TemplateFile .\ArmTemplates\composed\AppService\DeployWebApp.json `
+-TemplateParameterFile .\ArmTemplates\composed\AppService\Parameters\DeployWebApp.parameters.wow.dev.json -Verbose
