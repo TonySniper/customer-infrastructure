92985be2cca033894d0cf83c5421710aadd764e7
Antonio Risaldo <antonio.risaldo@gmail.com>
2020-08-14 16:20:37 +0200

    Added support for multiple backends in a backend pool; Added temp parameter file for test purposes; Created front door deployment PS script

--- Infrastructure/PowerShell/TestFrontDoorDeployment.ps1
+++ Infrastructure/PowerShell/TestFrontDoorDeployment.ps1
@@ -1,3 +1,4 @@
 ﻿New-AzResourceGroupDeployment -Name "TestDeployFrontDoor" `
 -TemplateFile .\ArmTemplates\nested\FrontDoor\FrontDoor.json `
+-TemplateParameterFile .\ArmTemplates\nested\FrontDoor\Parameters\DeployFrontDoor.parameters.wow.dev.json `
 -Verbose
