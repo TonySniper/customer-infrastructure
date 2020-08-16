26646e888ffd70191ac0c6de62670a3d86de76d0
Antonio Risaldo <antonio.risaldo@gmail.com>
2020-08-16 21:52:48 +0200

    Trying to fix issue with wrong database deployment name

---
+++ Infrastructure/PowerShell/TestMySqlDatabaseDeployment.ps1
@@ -0,0 +1,3 @@
+﻿New-AzResourceGroupDeployment -Name "TestDeploySQLDatabase" `
+-TemplateFile .\ArmTemplates\composed\Storage\DeployMySqlDatabase.json `
+-TemplateParameterFile .\ArmTemplates\composed\Storage\Parameters\DeployMySqlDatabase.parameters.wow.dev.json -Verbose
